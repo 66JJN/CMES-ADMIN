ba8621adb26c07fa208764fadb48340642c710c2
66JJNN <pyaksda@gmail.com>
2025-11-26 18:53:56 +0700

    แก้ไขบัค+ลบไฟล์ที่ไม่เกี่ยวข้อง

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./Register"; // นำเข้า Register
 import Home from "./home"; // นำเข้า Home
-import ProfileSetup from "./ProfileSetup"; // นำเข้า ProfileSetup
 import Report from "./AdminReport"; // นำเข้า Report
 import AdminStatSlip from "./Stat-slip"; // ชื่อ component ต้องตรงกับที่ export
 import ImageQueue from "./ImageQueue";
@@ -17,7 +16,6 @@ function App() {
         <Route path="/" element={<Register />} /> {/* หน้าแรกสุด */}
         <Route path="/home" element={<Home />} /> {/* หน้า Home */}
         <Route path="/report" element={<Report />} /> {/* หน้า Report */}
-        <Route path="/profile-setup" element={<ProfileSetup />} /> {/* เส้นทางใหม่ */}
         <Route path="/stat-slip" element={<AdminStatSlip />} />
         <Route path="/image-queue" element={<ImageQueue />} />
         <Route path="/TimeHistory" element={<TimeHistory />} />
